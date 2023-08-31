b932fac5d485138cbab0a6b3f678b057082123b0
Nathakk <137855783+Nathakk@users.noreply.github.com>
2023-08-31 21:01:29 +0700

    Add files via upload

---
+++ Game/app.js
@@ -0,0 +1,6 @@
+<scricpt>
+    let showscore = document.getElementById('score');       
+    let showpoint = document.getElementById('point');
+    let shownumber1 = document.getElementById(number1);
+
+</scricpt>
